c7263acd36f7c967e12ccb6a6e923095c3df12da
Dean S. Miller <dean.sylvester.miller@gmail.com>
2013-06-28 12:35:39 +0100

    Separated search from pagination

--- flickrsearch.js
+++ flickrsearch.js
@@ -1,17 +1,14 @@
 var FlickrSearch = (function($){
 
-	function FlickrSearch(apiKey){
+	function FlickrSearch(apiKey, perPage){
 		if(apiKey){
 			this.config = {
 				api_key: apiKey,
-				format: "json"
+				format: "json",
+				per_page: perPage
 			};
-			this.currentPage = 1;
-			this.minPage = 1; // least amount of page links
-			this.maxPages = 10; // max amount of page links 
-			this.firstPage = 1;
-			this.lastPage = this.maxPages;
-
+			this.perPage = perPage;
+			this.currentQuery = ""; //keep track of query
 		} else throw new Error("No API key provided");
 	}
 
@@ -21,27 +18,29 @@ var FlickrSearch = (function($){
 			this.photoContainer = $("#" + containerId);
 		},
 
-		setPaginationContainer: function(containerId){
-			this.paginationContainer = $("#" + containerId);
-			this.attachPaginationEventListeners(); //use LIVE
+		setPaginator: function(paginator){
+			this.paginator = paginator;
 		},
 
-		search: function(query, perPage, pageNo){
-			this.config["per_page"] = this.perPage = perPage || 8;
-			this.config["page"] = this.pageNo = pageNo || 1;
-			this.config["text"] = this.currentQuery = query;
-			this.currentPage = pageNo || 1;
+		search: function(query){
+			if($.type(query) === "object"){ //must be a pagination object
+				this.config["per_page"] = query.perPage;
+				this.config["page"] = query.pageNo;
+				this.config["text"] = this.currentQuery
+			} else this.config["text"] = this.currentQuery = query;
+
 			$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&jsoncallback=?", this.config, this.handleResults.bind(this));
 		},
 
 		handleResults: function(results){
 			if(results.stat === "ok"){
 				var photos = results.photos;
-				if(photos.photo.length > 0){
+				if(photos.photo.length > 0){ //if we have resuls
 					this.createImageElements(photos.photo);
-					this.createPagination();
-					this.totalPages = photos.pages;
-					console.log(photos);
+					if(this.paginator){ // if we have a paginator
+						this.paginator.setTotalPages(photos.pages);
+						this.paginator.createPagination(photos);
+					}
 				} else console.log("No results..");
 			} else if(results.stat === "fail"){
 				throw new Error(results.message);
@@ -59,112 +58,7 @@ var FlickrSearch = (function($){
 					class: "photos"
 				}).appendTo(container);
 			});
-		},
-
-		createPagination: function(){
-			if(this.paginationContainer){
-				var container = this.paginationContainer,
-					firstPage = this.firstPage,
-					lastPage = this.lastPage;
-
-				// create ul element for pagaintion if it does not exist
-				if(container.children("ul.pagination").length == 0){
-					$("<ul/>", {
-						class: "pagination"
-					}).appendTo(container);
-				} else {
-					// remove pagination as it will be re-calculated and rendered
-					$("ul.pagination").children().remove();
-				}
-
-				// add previous link
-				this.createPageElement({
-					html: "<p>Prev</p>",
-					class: "links",
-					move: "prev"
-				});				
-
-				// page links
-				for(var i = firstPage, max = lastPage; i <= max; i++){
-					this.createPageElement({
-						html: "<p>" + i + "</p>",
-						class: "page-element",
-						page: i
-					});
-				}
-
-				this.highlightPage(this.currentPage);
-
-				// add next link	
-				this.createPageElement({
-					html: "<p>Next</p>",
-					class: "links",
-					move: "next"
-				});
-				
-			} else throw new Error("No pagination container set.");
-		},
-
-		createPageElement: function(config){
-			$("<li/>", config).appendTo($("ul.pagination"));				
-		},
-
-		attachPaginationEventListeners: function(){
-			var self = this;
-
-			$(document).on("click", "ul.pagination li.page-element", function(e){
-				self.displayPages($(this));
-			});
-
-			$(document).on("click", "ul.pagination li.links", function(e){
-				self.moveToPage($(this).attr("move"));
-			});
-		},
-
-		highlightPage: function(page){
-			$(".pagination .page-element[page='" + page + "']").addClass("active");
-		},
-
-		//next or previous page links clicked
-		moveToPage: function(move){
-			var nextPage = 0,
-				currentPage = this.currentPage;
-
-			if(move === "next" && currentPage < this.totalPages){ 
-				nextPage = currentPage + 1; 
-				if(nextPage == this.lastPage){
-					this.displayPages($("ul.pagination li.page-element[page='" + nextPage + "']"));
-				}
-			} else if(move === "prev" && currentPage > this.minPage) { 
-				nextPage = currentPage - 1; 
-				if(nextPage == this.firstPage){
-					this.displayPages($("ul.pagination li.page-element[page='" + nextPage + "']"));
-				}
-			}
-
-			this.search(this.currentQuery, this.perPage, nextPage);
-		},
-
-		// calculates which pages to display
-		displayPages: function(pageElement){
-			this.currentPage = parseInt(pageElement.attr("page"));
-			var currentPage = this.currentPage,
-				firstPage = this.firstPage,
-				lastPage = this.lastPage,
-				maxPages = this.maxPages;
-
-			if(currentPage === lastPage){
-				this.firstPage = (lastPage - Math.floor(maxPages / 2)) + 1;
-				this.lastPage = lastPage + Math.floor(maxPages / 2);	
-			} else if(currentPage === firstPage  && currentPage !== this.minPage){
-				this.lastPage = firstPage + Math.floor(maxPages / 2) - 1;
-				this.firstPage = firstPage - Math.floor(maxPages / 2);	
-			}
-
-			this.search(this.currentQuery, this.perPage, currentPage);
 		}
-
-
 	};
 
 
---
+++ paginator.js
@@ -0,0 +1,150 @@
+var Paginator = (function($){
+
+	function Paginator(container, perPage){
+		this.currentPage = 1;
+		this.minPage = 1; // least amount of page links
+		this.maxPages = 10; // max amount of page links 
+		this.firstPage = 1;
+		this.lastPage = this.maxPages;
+		this.callback = null; //called after each page click
+		this.perPage = perPage || 4;
+		this.paginationContainer = $("#" + container);
+		this.attachPaginationEventListeners();
+	}
+
+	Paginator.prototype = {
+
+		setTotalPages: function(totalPages){
+			this.totalPages = totalPages;
+		},
+
+		setPerPage: function(perPage){
+			this.perPage = perPage;
+		},
+
+		//create pagination elements, create next/previous page links and numbered links in between
+		createPagination: function(){
+			var container = this.paginationContainer,
+				firstPage = this.firstPage,
+				lastPage = this.lastPage;
+
+			// create ul element for pagaintion if it does not exist
+			if(container.children("ul.pagination").length == 0){
+				$("<ul/>", {
+					class: "pagination"
+				}).appendTo(container);
+			} else {
+				// remove pagination as it will be re-calculated and rendered
+				$("ul.pagination").children().remove();
+			}
+
+			// add previous link
+			this.createPageElement({
+				html: "<p>Prev</p>",
+				class: "links",
+				id: "prevPageLink",
+				move: "prev"
+			});				
+
+			// page links
+			for(var i = firstPage, max = lastPage; i <= max; i++){
+				this.createPageElement({
+					html: "<p>" + i + "</p>",
+					class: "page-element",
+					page: i
+				});
+			}
+
+			this.highlightPageLink(this.currentPage);
+
+			// add next link	
+			this.createPageElement({
+				html: "<p>Next</p>",
+				class: "links",
+				id: "nextPageLink",
+				move: "next"
+			});
+				
+		},
+
+		// called on page element click
+		onPageClick: function(callback){
+			this.callback = callback;
+		},
+
+		createPageElement: function(config){
+			$("<li/>", config).appendTo($("ul.pagination"));				
+		},
+
+		attachPaginationEventListeners: function(){
+			var self = this;
+
+			$(document).on("click", "ul.pagination li.page-element", function(e){
+				var paginationConfig = self.displayPages($(this));
+				self.callback(paginationConfig);
+			});
+
+			$(document).on("click", "ul.pagination li.links", function(e){
+				var paginationConfig = self.moveToPage($(this).attr("move"));
+				self.callback(paginationConfig);
+			});
+		},
+
+		// highlight active page
+		highlightPageLink: function(page){
+			$(".pagination .page-element[page='" + page + "']").addClass("active");
+		},
+
+		//next or previous page links clicked, move to the next or previous page
+		moveToPage: function(move){
+			var nextPage = 0,
+				currentPage = this.currentPage;
+
+			if(move === "next" && currentPage < this.totalPages){ //stay in bounds
+				nextPage = currentPage + 1; 
+				if(nextPage == this.lastPage){
+					this.displayPages($("ul.pagination li.page-element[page='" + nextPage + "']"));
+				}
+			} else if(move === "prev" && currentPage > this.minPage) { 
+				nextPage = currentPage - 1;
+				if(nextPage == this.firstPage){
+					this.displayPages($("ul.pagination li.page-element[page='" + nextPage + "']"));
+				}
+			}
+
+			this.currentPage = nextPage; // keep track of current page
+			
+			return {
+				perPage: this.perPage,
+				pageNo: nextPage
+			}
+		},
+
+		// calculates which pages to display, when first or last page link clicked
+		// works out where the middle is and what the first and last page links should be
+		displayPages: function(pageElement){
+			this.currentPage = parseInt(pageElement.attr("page"));
+			var currentPage = this.currentPage,
+				firstPage = this.firstPage,
+				lastPage = this.lastPage,
+				maxPages = this.maxPages;
+
+			if(currentPage === lastPage){
+				this.firstPage = (lastPage - Math.floor(maxPages / 2)) + 1;
+				this.lastPage = lastPage + Math.floor(maxPages / 2);	
+			} else if(currentPage === firstPage  && currentPage !== this.minPage){
+				this.lastPage = firstPage + Math.floor(maxPages / 2) - 1;
+				this.firstPage = firstPage - Math.floor(maxPages / 2);	
+			}
+
+			return {
+				perPage: this.perPage,
+				pageNo: currentPage
+			}
+		}
+
+	};
+
+	return Paginator;
+
+})(jQuery);
